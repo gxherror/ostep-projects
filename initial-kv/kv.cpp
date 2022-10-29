0cffe5aee55c968519f4a6840164c25fd374f167
xherror_ubuntu <xherror@foxmail.com>
2022-10-29 12:52:49 +0800

    finish kv without persistence

--- initial-kv/kv.cpp
+++ initial-kv/kv.cpp
@@ -1,35 +1,43 @@
 #include<stdio.h>
 #include<string.h>
 #include<iostream>
-#define LISTSIZE 100
+#define LISTSIZE 2
 using namespace std;
 
-struct node 
+struct Node 
 {
+    string key;
     string value;
-    node* ptr;
+    Node* ptr;
 };
+
+struct HeadNode
+{
+    Node* ptr;
+};
+
 
 class HashTable
 {
 private:
-    node* bptr;
+    HeadNode* bptr;
 
     //get key and return value's address
+    //add type hash
     int hash(string key){
         int hash, i;
         for (hash = key.length(), i = 0; i < key.length(); i++){
              hash += key[i];
         };
         return (hash % LISTSIZE);
-    }
+    };
+
 public:
 
     HashTable(int size){
-        bptr = new node[size];
+        bptr = new HeadNode[size];
         for (int i=0;i<LISTSIZE;i++){
-        bptr[i].value = "";
-        bptr[i].ptr = NULL;
+            bptr[i].ptr = NULL;
         }
     };
 
@@ -40,32 +48,75 @@ public:
     void add(string key,string value){
         int position = hash(key);
         cout<<"position is : "<<position<<'\n';
-        if (bptr[position].value=="" && bptr[position].ptr==NULL){
-            bptr[position].value=value;
-        }else if (bptr[position].ptr== NULL){
-            node* next = new node;
-            next->value="";
+        HeadNode* headNode=&bptr[position];
+        //first node is empty
+        if (headNode->ptr==NULL){
+            Node* newNode = new Node;
+            newNode->key=key;
+            newNode->value=value;
+            newNode->ptr=NULL;
+            headNode->ptr=newNode;
         }else{
-
+            Node* currentNode=headNode->ptr;
+            for (;currentNode->ptr!=NULL;currentNode=currentNode->ptr){};
+            //first node have been occupied , find the end of linked list
+            //and add a new node to the end of linked list,  
+            Node* newNode = new Node;
+            newNode->key=key;
+            newNode->value=value;
+            newNode->ptr=NULL;
+            currentNode->ptr=newNode;
         }
-        
-        
     };
 
     string get(string key){
-
+        int position = hash(key);
+        Node* currentNode=bptr[position].ptr;
+        for (;currentNode!=NULL;currentNode=currentNode->ptr){
+            if (currentNode->key==key){
+                return currentNode->value;
+            };
+        };
+        return "";
     };
 
     void del(string key){
+        int position = hash(key);
+        Node* currentNode=bptr[position].ptr;
+        if (currentNode->key==key){
+            bptr[position].ptr=currentNode->ptr;
+            free(currentNode);
+        }else{
+            for (;currentNode->ptr!=NULL;currentNode=currentNode->ptr){
+                //if next node's key match the target ,delete next node 
+                //and change currentNode ptr
+                if (currentNode->ptr->key==key){
+                    currentNode->ptr=currentNode->ptr->ptr;
+                    free(currentNode->ptr);
+                };
+            };
+        }
+        
 
     };
 
+
+    void show(){
+        for (int i=0;i<LISTSIZE;++i){
+            cout<<bptr[i].ptr;
+            Node* currentNode=bptr[i].ptr;
+            for (;currentNode!=NULL;currentNode=currentNode->ptr){
+                cout<<" -> "<<" key : "<<currentNode->key<<" , value: "<<currentNode->value;
+            };
+            cout<<'\n';
+        }
+    };
 };
 
 
 int main(int argc,char *argv[]){
 
-    HashTable hashtable(LISTSIZE);
+    HashTable hashTable(LISTSIZE);
 
     if (argc==1){
         cout<<"not enough args";
@@ -81,14 +132,17 @@ int main(int argc,char *argv[]){
         }
         if (args[0]=="p"){
             printf("begin process put...\n");
-            hashtable.add(args[1],args[2]);
+            hashTable.add(args[1],args[2]);
         }
         if (args[0]=="g"){
-
+            printf("begin process get...\n");
+            cout<<"value is : "<<hashTable.get(args[1])<<'\n';
         }
         if (args[0]=="d"){
-
+            printf("begin process del...\n");
+            hashTable.del(args[1]);
         }
     }
+    hashTable.show();
     return 0;
 }
